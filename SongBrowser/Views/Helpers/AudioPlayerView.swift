7ff38fbb9f95da8d11c9dbf69b00da9505108dde
Russell Gordon <github@russellgordon.ca>
2022-05-05 06:26:20 -0400

    Refactored audio player so that only one button shows at a time.

--- SongBrowser/Views/Helpers/AudioPlayerView.swift
+++ SongBrowser/Views/Helpers/AudioPlayerView.swift
@@ -9,55 +9,60 @@ import AVKit
 import SwiftUI
 
 // ADAPTED FROM SOURCE: https://medium.com/swift-productions/swiftui-play-an-audio-with-avaudioplayer-1c4085e2052c
+//
+// NOTE: It is also possible to play audio from files in the app bundle.
 
 struct AudioPlayerView: View {
     
     // MARK: Stored properties
+    
+    // Used to play the audio
     @State var audioPlayer: AVPlayer = AVPlayer()
-    var urlOfSongToPlay: String
+    
+    // What remote link
+    let urlOfSongToPlay: String
+    
+    // Whether a song is playing
+    @State var isSongPlaying = false
     
     // MARK: Stored properties
     var body: some View {
         
-        VStack {
+        ZStack {
             
-            HStack {
-                
-                Spacer()
-                
-                Button(action: {
-                    self.audioPlayer.play()
-                }) {
-                    Image(systemName: "play.circle.fill")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .aspectRatio(contentMode: .fit)
-                }
-                
-                Spacer()
-                
-                Button(action: {
-                    self.audioPlayer.pause()
-                }) {
-                    Image(systemName: "pause.circle.fill")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .aspectRatio(contentMode: .fit)
-                }
-                
-                Spacer()
+            Button(action: {
+                audioPlayer.play()
+                isSongPlaying = true
+            }) {
+                Image(systemName: "play.circle.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .aspectRatio(contentMode: .fit)
             }
+            // Visible only when song is not playing
+            .opacity(isSongPlaying == false ? 1.0 : 0.0)
             
+            Button(action: {
+                audioPlayer.pause()
+                isSongPlaying = false
+            }) {
+                Image(systemName: "pause.circle.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .aspectRatio(contentMode: .fit)
+            }
+            // Visible only when song IS playing
+            .opacity(isSongPlaying == true ? 1.0 : 0.0)
         }
         .onAppear {
             
             // Create a URL instance from the preview URL
             let url = URL(string: urlOfSongToPlay)!
             
-            // Make an instance of the AVPlayerItem class
+            // Make an instance of AVPlayerItem
             let playerItem = AVPlayerItem(url: url)
             
-            // Set the player
+            // Reset instance of AVPlayer
             audioPlayer = AVPlayer(playerItem: playerItem)
             
             // Set the volume
